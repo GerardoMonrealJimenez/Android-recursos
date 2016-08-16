f99a164c3e8d293ca6fcf02130cbe274d5db51d4
GerardoMonrealJimenez <gerardo.monreal.imt@gmail.com>
2016-08-16 00:19:03 -0500

    Android recursos
    Version1

---
+++ AndroidRecursos/app/src/main/java/mx/com/rteck/androidrecursos/Bienvenidos.java
@@ -0,0 +1,13 @@
+package mx.com.rteck.androidrecursos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bienvenidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenidos);
+    }
+}
